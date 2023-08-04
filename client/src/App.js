9a235bb8e80a880901d25edd8598b4c1c989879b
ulasdemir472 <ulasdemir472@gmail.com>
2023-08-04 22:48:35 +0300

    bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import MainPage from "./pages/MainPage";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Register2 from "./pages/Register2";
+import RegisterDog from "./pages/RegisterDog";
 import "./style.scss";
 import { Home } from "./pages/Home/Home";
 import MatchesContainer from "./components/Matches/MatchesContainer";
@@ -13,7 +13,7 @@ function App() {
       <Route path="/" element={<MainPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/registerDog" element={<Register2 />} />
+      <Route path="/registerDog" element={<RegisterDog />} />
       <Route path="/home" element={<Home />} />
       <Route path="/matches" element={<MatchesContainer />} />
     </Routes>
